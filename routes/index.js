dba528162b0cd6025e8b61786d71733f791b11d8
Rajashree Parhi <rajashreeparhi23@gmail.com>
2019-02-21 21:28:22 +0530

    Added csv file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+
+
 });
 
+router.post('/login', function(req, res) {
+
+        res.redirect('index');
+
+});
 module.exports = router;
